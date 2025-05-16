03249e88b76d9e4b89d2d4fa0479c8001ad16fdd
ryanyeap <ryanyeap@hotmail.com>
2025-05-16 12:38:18 +0800

    Auth UI

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,13 +1,17 @@
-import {Link, NavLink} from "react-router";
+import {Link, NavLink, useLoaderData, useNavigate} from "react-router";
 import {sidebarItems} from "~/constants";
 import {cn} from "~/lib/utils";
+import {logoutUser} from "~/appwrite/auth";
 
 const NavItems = ({ handleClick } : { handleClick: () => void}) => {
-    const user = {
-        name: 'Ryan',
-        email: 'ryanyeap@hotmail.com',
-        imageUrl: '/assets/images/david.webp'
+    const user = useLoaderData();
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        await logoutUser();
+        navigate('/sign-in');
     }
+
     return (
         <section className="nav-items">
             <Link to='/' className="link-logo">
@@ -36,26 +40,25 @@ const NavItems = ({ handleClick } : { handleClick: () => void}) => {
                 </nav>
 
                 <footer className="nav-footer">
-                    <img src={user?.imageUrl || 'assets/images/david.webp'} alt={user?.name || 'Ryan'}/>
+                    <img src={user?.imageUrl || 'assets/images/david.webp'} alt={user?.name || 'Undefined'} referrerPolicy="no-referrer"/>
+
+                    <article>
+                        <h2>{user?.name}</h2>
+                        <p>{user?.email}</p>
+                    </article>
+
+                    <button
+                        onClick={handleLogout}
+                        className="cursor-pointer"
+                    >
+                        <img
+                            src="/assets/icons/logout.svg"
+                            alt="Logout"
+                            className="size-6"
+                        />
+                    </button>
                 </footer>
 
-                <article>
-                    <h2>{user?.name}</h2>
-                    <p>{user?.email}</p>
-                </article>
-
-                <button
-                    onClick={() => {
-                        console.log('logout')
-                    }}
-                    className="cursor-pointer"
-                >
-                    <img
-                        src="/assets/icons/logout.svg"
-                        alt="Logout"
-                        className="size-6"
-                    />
-                </button>
             </div>
         </section>
     )
