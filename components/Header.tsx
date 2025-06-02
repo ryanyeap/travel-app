98703ab9aa6ba04681bb8ac18193b3c326ecaa52
ryanyeap <ryanyeap@hotmail.com>
2025-06-03 07:03:26 +0800

    Add New Trip Form

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,15 @@
-import { useLocation } from "react-router";
+import {Link, useLocation} from "react-router";
 import {cn} from "~/lib/utils";
+import {ButtonComponent} from "@syncfusion/ej2-react-buttons";
 
 interface Props {
     title: string,
     description: string,
+    ctaText?: string,
+    ctaUrl?: string,
 }
 
-const Header = ({ title, description }: Props) => {
+const Header = ({ title, description, ctaText, ctaUrl }: Props) => {
     const location = useLocation();
 
     return (
@@ -19,6 +22,15 @@ const Header = ({ title, description }: Props) => {
                     {description}
                 </p>
             </article>
+
+            {ctaText && ctaUrl && (
+                <Link to={ctaUrl}>
+                    <ButtonComponent type="button" className="button-class !h-11 !w-full md:w-[240px]">
+                        <img src="/assets/icons/plus.svg" className="size-5" alt="plus" />
+                        <span className="p-16-semibold text-white">{ctaText}</span>
+                    </ButtonComponent>
+                </Link>
+            )}
         </header>
     )
 }
